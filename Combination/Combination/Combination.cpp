ab9c4467682b1a129af99ef792199eb827ede151
장원 <jangwonpark74@gmail.com>
2016-09-11 07:55:52 +0900

    add initialization

--- Combination/Combination/Combination.cpp
+++ Combination/Combination/Combination.cpp
@@ -1,18 +1,28 @@
 #include <stdio.h>
 
-int select[6]; 
-int visited[6];
+struct item {
+	int visited;
+	int value;
+	item() {}
+	item(int visited_, int value_) : visited(visited_), value(value_) {};
+	int isVisited() { return visited; };
+	void init() { visited = 0; value = 0; };
+	int getValue() { return value; };
+};
+
+item select[10];
+
 int cnt;
 
-void combination(int arr[], int n, int r, int t)
+void combination(int arr[], int n, int r, int t, item select[])
 {
 	if (r == 0)
 	{
 		printf("[%d] ", cnt++);
-		for (int i = 1; i <=t; i++)
+		for (int i = 1; i <= t; i++)
 		{
-			if(visited[i] == 1)
-			printf("%d ", select[i]);
+			if (select[i].isVisited())
+				printf("%d ", select[i].getValue());
 		}
 
 		printf("\n");
@@ -27,10 +37,9 @@ void combination(int arr[], int n, int r, int t)
 	}
 	else
 	{
-		visited[r] = 1;
-		select[r] = arr[t];
-		combination(arr, n - 1, r - 1, t+1);
-		combination(arr, n - 1, r, t+1);
+		select[r] = item(1, arr[t]);
+		combination(arr, n - 1, r - 1, t+1, select);
+		combination(arr, n - 1, r, t+1, select);
 	}
 
 }
@@ -40,14 +49,23 @@ int main()
 	int arr[] = { 1, 3, 5, 7, 9 };
 
 	int n = sizeof(arr) / sizeof(arr[0]);
+	
+	printf("\ninit & print\n");
+
+	for (int i = 0; i < 10; i++)
+		select[i]= item(0, 0);
 
 	for (int k = 1; k <= 5; k++)
 	{
+		
 		cnt = 1;
 		printf(" 5C%d example\n", k);
-		combination(arr, n, k, 0);
+		combination(arr, n, k, 0, select);
 		printf("\n\n");
-	}
 
+		for (int i = 0; i < 10; i++)
+			select[i] = item(0, 0);
+	
+	}
 	return 0;
 }
