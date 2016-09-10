11bcd5ebf97a6fd211db5acf0d514a5db8a02209
장원 <jangwonpark74@gmail.com>
2016-09-10 23:01:24 +0900

    Add powerset by counting

--- Subset/Subset/Subset.cpp
+++ Subset/Subset/Subset.cpp
@@ -20,10 +20,32 @@ void powerset(int arr[],int n, int max)
 	}
 }
 
+void subset()
+{
+	int i, j;
+	int arr[] = { 3, 6, 7, 1, 5, 4 };
+	int n= sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < (1 << n); i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (i & (1 << j))
+				printf("%d ", arr[j]);
+		}
+		printf("\n"); 
+	}
+}
+
 int main()
 {
 	int arr[5];
 	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	printf("\n# powerset(1) #\n");
 	powerset(arr, 0, arr_size);
+
+	printf("\n# powerset(2) #\n");
+	subset();
 	return 0;
 }
