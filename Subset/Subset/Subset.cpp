5f7407c1658067e45a1ffeb40269eb5cbdfbd844
장원 <jangwonpark74@gmail.com>
2016-09-10 23:07:46 +0900

    remove local variable declaration

--- Subset/Subset/Subset.cpp
+++ Subset/Subset/Subset.cpp
@@ -22,13 +22,12 @@ void powerset(int arr[],int n, int max)
 
 void subset()
 {
-	int i, j;
 	int arr[] = { 3, 6, 7, 1, 5, 4 };
 	int n = sizeof(arr) / sizeof(arr[0]);
 
 	for (int i = 0; i < (1 << n); i++)
 	{
-		for (j = 0; j < n; j++)
+		for (int j = 0; j < n; j++)
 		{
 			if (i & (1 << j))
 				printf("%d ", arr[j]);
